4f812e7511abfd33d9715f84ee6676edfe20d9ba
ohinibla <ohinibla@gmail.com>
2024-12-12 22:01:14 +0330

    ✨ (noirbuddy.lua): integrate noirbuddy theme into lualine configuration for improved aesthetics ♻️ (noirbuddy.lua): refactor lualine sections to include show_macro_recording function for enhanced functionality

--- colors/noirbuddy.lua
+++ colors/noirbuddy.lua
@@ -4,23 +4,14 @@ require('noirbuddy').setup {
 
 local noirbuddy_lualine = require('noirbuddy.plugins.lualine')
 
+lvim.builtin.lualine.options.theme = noirbuddy_lualine.theme
 lvim.builtin.lualine.sections = noirbuddy_lualine.sections
 lvim.builtin.lualine.inactive_sections = noirbuddy_lualine.inactive_sections
 
--- add selctioncount to lualine
-local show_selection_count = require("user.functions").show_selection_count
--- local show_macro_recording = require("user.functions").show_macro_recording
-local components = require("lvim.core.lualine.components")
 
--- Show @recording messages
--- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
-lvim.builtin.lualine.sections.lualine_x = {
-	{
-		require("noice").api.statusline.mode.get,
-		cond = require("noice").api.statusline.mode.has,
-		color = { fg = "#ff9e64" },
-	},
-}
+local show_selection_count = require("user.functions").show_selection_count
+local show_macro_recording = require("user.functions").show_macro_recording
+local components = require("lvim.core.lualine.components")
 
 lvim.builtin.lualine.sections.lualine_y = {
 	components.location,
@@ -29,5 +20,8 @@ lvim.builtin.lualine.sections.lualine_y = {
 		cond = function()
 			return vim.fn.mode():find("[vv]") ~= nil
 		end,
+	},
+	{
+	show_macro_recording,
 	},
 }
