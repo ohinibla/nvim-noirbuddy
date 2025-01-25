9f7f1e2af62910a25ae9c594f28ac3094076df1f
ohinibla <ohinibla@gmail.com>
2025-01-25 12:35:22 +0330

    ✨ (noirbuddy.lua): update preset from 'crt-amber' to 'slate' for improved aesthetics ♻️ (noirbuddy.lua): refactor lualine sections for better organization and clarity

--- colors/noirbuddy.lua
+++ colors/noirbuddy.lua
@@ -1,27 +1,31 @@
-require('noirbuddy').setup {
-  preset = 'crt-amber',
-}
-
-local noirbuddy_lualine = require('noirbuddy.plugins.lualine')
-
-lvim.builtin.lualine.options.theme = noirbuddy_lualine.theme
-lvim.builtin.lualine.sections = noirbuddy_lualine.sections
-lvim.builtin.lualine.inactive_sections = noirbuddy_lualine.inactive_sections
+require("noirbuddy").setup({
+	preset = "slate",
+})
 
 
 local show_selection_count = require("user.functions").show_selection_count
 local show_macro_recording = require("user.functions").show_macro_recording
+local noirbuddy_lualine = require("noirbuddy.plugins.lualine")
 local components = require("lvim.core.lualine.components")
 
-lvim.builtin.lualine.sections.lualine_y = {
-	components.location,
-	{
-		show_selection_count,
-		cond = function()
-			return vim.fn.mode():find("[vv]") ~= nil
-		end,
+local sections = {
+	lualine_a = { "mode" },
+	lualine_b = { "branch", "diff" },
+	lualine_c = { { "filename", path = 1 } },
+	lualine_x = { components.lsp, "diagnostics", { "filetype", colored = false }, "encoding" },
+	lualine_y = {
+		"progress",
+		{
+			show_selection_count,
+			cond = function()
+				return vim.fn.mode():find("[vv]") ~= nil
+			end,
+		},
+		show_macro_recording,
 	},
-	{
-	show_macro_recording,
-	},
+	lualine_z = { "location" },
 }
+
+lvim.builtin.lualine.options.theme = noirbuddy_lualine.theme
+lvim.builtin.lualine.sections = sections
+lvim.builtin.lualine.inactive_sections = noirbuddy_lualine.inactive_sections
